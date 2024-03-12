d0643921419a2c5f92c89922d17a6680c4353998
Louis Pilfold <louis@lpil.uk>
2024-03-12 22:26:39 +0000

    Fix unicode handling
    Closes https://github.com/lpil/envoy/issues/1

--- src/envoy_ffi.erl
+++ src/envoy_ffi.erl
@@ -3,22 +3,22 @@
 -export([get/1, set/2, unset/1, all/0]).
 
 get(Key) ->
-    case os:getenv(binary_to_list(Key)) of
+    case os:getenv(unicode:characters_to_list(Key)) of
         false -> {error, nil};
-        Value -> {ok, list_to_binary(Value)}
+        Value -> {ok, unicode:characters_to_binary(Value)}
     end.
 
 set(Key, Value) ->
-    os:putenv(binary_to_list(Key), binary_to_list(Value)),
+    os:putenv(unicode:characters_to_list(Key), unicode:characters_to_list(Value)),
     nil.
 
 unset(Key) ->
-    os:unsetenv(binary_to_list(Key)),
+    os:unsetenv(unicode:characters_to_list(Key)),
     nil.
 
 all() ->
     BinVars = lists:map(fun(VarString) ->
         [VarName, VarVal] = string:split(VarString, "="),
-        {list_to_binary(VarName), list_to_binary(VarVal)}
+        {unicode:characters_to_binary(VarName), unicode:characters_to_binary(VarVal)}
     end, os:getenv()),
     maps:from_list(BinVars).
--- test/envoy_test.gleam
+++ test/envoy_test.gleam
@@ -1,6 +1,6 @@
-import gleam/io
-import gleam/dict
 import envoy
+import gleam/dict
+import gleam/io
 
 pub fn main() {
   // All
@@ -20,6 +20,12 @@ pub fn main() {
   let assert Error(_) = envoy.get("UAGE")
   let all = envoy.all()
   let assert Error(_) = dict.get(all, "UAGE")
+
+  // Unicode
+  envoy.set("WIBBLE", "🦄")
+  let assert Ok("🦄") = envoy.get("WIBBLE")
+  let all = envoy.all()
+  let assert Ok("🦄") = dict.get(all, "WIBBLE")
 
   io.println("All good sweetie 💕")
 }
